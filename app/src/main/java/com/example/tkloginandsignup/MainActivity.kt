00478b0ced0b7e3332ce0a9da876784d529f2c0e
Kazenzi <jacintakazenzi99@gmail.com>
2023-11-20 18:47:11 +0300

    Clickable button

--- app/src/main/java/com/example/tkloginandsignup/MainActivity.kt
+++ app/src/main/java/com/example/tkloginandsignup/MainActivity.kt
@@ -1,11 +1,28 @@
 package com.example.tkloginandsignup
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+//        accesbutton
+       // val joinButton:Button =findViewById(R.id.joinnow)
+        val joinbtn : Button = findViewById(R.id.joinnow)
+//        setlistner
+        joinbtn.setOnClickListener {
+//            navigatetologin
+            val intent=Intent(
+                this,
+                LogInActivity::class.java
+
+            )
+            startActivity(intent)
+        }
+
     }
 }
