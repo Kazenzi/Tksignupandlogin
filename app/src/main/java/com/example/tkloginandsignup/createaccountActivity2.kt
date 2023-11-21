8c01d941ef3d20186e3035b51a39b14cab346715
Kazenzi <jacintakazenzi99@gmail.com>
2023-11-21 14:35:53 +0300

    createaccount

---
+++ app/src/main/java/com/example/tkloginandsignup/createaccountActivity2.kt
@@ -0,0 +1,11 @@
+package com.example.tkloginandsignup
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class createaccountActivity2 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_createaccount2)
+    }
+}
