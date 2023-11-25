9cbc62ce4879de9ab6cb8c372e998ed2b3293e06
Kazenzi <jacintakazenzi99@gmail.com>
2023-11-25 21:11:59 +0300

    user restriction

---
+++ app/src/main/java/com/example/tkloginandsignup/HomeMainActivity2.kt
@@ -0,0 +1,11 @@
+package com.example.tkloginandsignup
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class HomeMainActivity2 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home_main2)
+    }
+}
--- app/src/main/java/com/example/tkloginandsignup/LogInActivity.kt
+++ app/src/main/java/com/example/tkloginandsignup/LogInActivity.kt
@@ -1,11 +1,37 @@
 package com.example.tkloginandsignup
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
 
 class LogInActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_log_in)
+
+        val forgotpassword:TextView=findViewById(R.id.textView3)
+        val logbth:Button=findViewById(R.id.button)
+        val userjina:EditText=findViewById(R.id.editTextTextPersonName)
+        val usersiri:EditText=findViewById(R.id.editTextTextPersonName2)
+
+//        setlistener
+        logbth.setOnClickListener {
+            val userjina=userjina.text.toString()
+            val usersiri=usersiri.text.toString()
+
+            if(
+                userjina.isNotEmpty() && usersiri.isNotEmpty()
+            ){
+            val intent=Intent(this,
+                HomeMainActivity2::class.java)
+            startActivity(intent)
+        }else{
+            Toast.makeText(this,"Please input all fields",Toast.LENGTH_SHORT).show()
+        }
+        }
     }
 }
